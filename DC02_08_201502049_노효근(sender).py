35c29ecab40c972f2c295649c219db34847fc3ba
Nroot_SW_33 <jessica0533@gmail.com>
2019-05-15 15:07:19 +0900

    Update DC02_08_201502049_노효근(sender).py

--- DC02_08_201502049_노효근(sender).py
+++ DC02_08_201502049_노효근(sender).py
@@ -1,6 +1,6 @@
 import socket 
 import os
-
+import time as t
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 s.bind(('',6000))
 
@@ -21,6 +21,7 @@ with open(name, 'rb') as send_file: #name이란 파일을, 읽기전용형식 "r
     for r in range(0,total_size,1024):
         current_size += 1024
         line = send_file.read(1024)
+        t.sleep(0.2)
         s.sendto(line,(host,int(port)))
         percent = current_size/total_size*100;
         if(percent > 100):
